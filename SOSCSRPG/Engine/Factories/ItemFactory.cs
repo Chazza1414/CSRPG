a1dc8c399309ca14160daf3c8622d6e957c24ab4
Charlie <69241683+Chazza1414@users.noreply.github.com>
2020-09-03 19:49:11 +0100

    Add files via upload

---
+++ SOSCSRPG/Engine/Factories/ItemFactory.cs
@@ -0,0 +1,44 @@
+﻿using Engine.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Engine.Factories
+{
+    public static class ItemFactory
+    {
+        private static readonly List<GameItem> _standardGameItems = new List<GameItem>();
+
+        static ItemFactory()
+        {
+            _standardGameItems = new List<GameItem>();
+
+            _standardGameItems.Add(new Weapon(1001, "Pointy Stick", 1, 1, 2));
+            _standardGameItems.Add(new Weapon(1002, "Rusty Sword", 5, 1, 3));
+            _standardGameItems.Add(new GameItem(9001, "Snake Fang", 1));
+            _standardGameItems.Add(new GameItem(9002, "Snake Skin", 2));
+            _standardGameItems.Add(new GameItem(9003, "Rat Tail", 1));
+            _standardGameItems.Add(new GameItem(9004, "Rat Fur", 2));
+            _standardGameItems.Add(new GameItem(9005, "Spider Fang", 1));
+            _standardGameItems.Add(new GameItem(9006, "Spider Silk", 2));
+        }
+
+        public static GameItem CreateGameItem(int itemTypeID)
+        {
+            GameItem standardItem = _standardGameItems.FirstOrDefault(item => item.ItemTypeID == itemTypeID);
+
+            if (standardItem != null)
+            {
+                if (standardItem is Weapon)
+                {
+                    return (standardItem as Weapon).Clone();
+                }
+                return standardItem.Clone();
+            }
+
+            return null;
+
+        }
+    }
+}
---
+++ SOSCSRPG/Engine/Models/Monster.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace Engine.Models
+{
+    public class Monster : BaseNotificationClass
+    {
+        private int _hitPoints;
+
+        public string Name { get; set; }
+        public string ImageName { get; set; }
+        public int MaximumHitPoints { get; set; }
+        public int HitPoints
+        {
+            get { return _hitPoints; }
+            set
+            {
+                _hitPoints = value;
+                OnPropertyChanged(nameof(HitPoints));
+            }
+        }
+
+        public int MinimumDamage { get; set; }
+        public int MaximumDamage { get; set; }
+
+        public int RewardExperiencePoints { get; private set; }
+        public int RewardGold { get; private set; }
+
+        public ObservableCollection<ItemQuantity> Inventory { get; set; }
+
+        public Monster(string name, string imageName,
+            int maximumHitpoints, int hitPoints,
+            int minimumDamage, int maximumDamage,
+            int rewardExperiencePoints, int rewardGold)
+        {
+            Name = name;
+            ImageName = string.Format($"E:/Coding/Game/SOSCSRPG/Engine/Images/Monsters/{imageName}");
+            MaximumHitPoints = maximumHitpoints;
+            HitPoints = hitPoints;
+            MaximumDamage = maximumDamage;
+            MinimumDamage = minimumDamage;
+            RewardExperiencePoints = rewardExperiencePoints;
+            RewardGold = rewardGold;
+
+            Inventory = new ObservableCollection<ItemQuantity>();
+        }
+    }
+}
